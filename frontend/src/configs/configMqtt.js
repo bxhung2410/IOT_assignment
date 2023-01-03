78db5eed41a04b122bd5c42dd5639c0a6ce37b4a
iris2410 <iris@vinova.com.sg>
2023-01-03 23:16:59 +0700

    upload code

---
+++ frontend/src/configs/configMqtt.js
@@ -0,0 +1,21 @@
+
+export const AIO_USERNAME = "bxhIdris"
+export const AIO_KEY = "aio_GQRt374R1E9VndesgmmLLQclYDYL"
+
+export const FEED_TEMP = AIO_USERNAME + "/feeds/warehouse-management.bbc-temp"
+export const FEED_HUMIDITY = AIO_USERNAME + "/feeds/warehouse-management.bbc-humi"
+export const FEED_LIGHT = AIO_USERNAME + "/feeds/warehouse-management.bbc-light"
+export const FEED_GAS = AIO_USERNAME + "/feeds/warehouse-management.bbc-gas"
+export const FEED_CONDITION = AIO_USERNAME + "/feeds/warehouse-management.bbc-condition"
+export const FEED_LED = AIO_USERNAME + "/feeds/warehouse-management.bbc-led"
+export const FEED_DOOR = AIO_USERNAME + "/feeds/warehouse-management.bbc-door"
+
+export const topics = [
+  FEED_TEMP,
+  FEED_HUMIDITY,
+  FEED_LIGHT,
+  FEED_GAS,
+  FEED_CONDITION,
+  FEED_LED,
+  FEED_DOOR,
+]
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,17 @@
+export const mqttHost = "https://io.adafruit.com/bxhIdris/groups/warehouse-management";
+export const serverhost = "http://localhost:8080";
+
+export const pi = Math.PI;
+export const tau = 2 * pi;
+
+export const HOURLY = 'hourly';
+export const DAILY = 'daily';
+export const MONTHLY = 'monthly';
+export const WEEKLY = 'weekly';
+
+export const ControlType = {
+  ON: 'ON',
+  OFF: 'OFF',
+  RISE: 'RISE',
+  FALL: 'FALL',
+}
